0738c36ab2ebd0a454afd81a19012050f71e8fad
Yariangel Aray <gabrielamunar4@gmail.com>
2024-12-02 07:59:50 -0500

    segundo ejercicio

---
+++ javascript/ejercicio02.js
@@ -0,0 +1,16 @@
+// Definimos una contraseña
+const contraseña = "yari0421";
+// Pedir la contraseña
+let contraseñaIgresada = prompt("Ingrese su contraseña: ").toLocaleLowerCase();
+// Expresión regular
+let regex = /^(\w)+$/;
+
+// Evaluamos que con al expresión regular que la contraseña solo tenga alfanuméricos
+if (!regex.test(contraseñaIgresada)) {
+    alert("La contraseña solo debe contener caracteres alfanuméricos.")
+}else if(contraseña == contraseñaIgresada){ // Si la contraseña contiene alfanumericos, devolvemos el valor falso para que ahora se evalue que ambas contraseñas sean iguales
+    alert("La contraseña es correcta.");
+}
+else{ // Por el contrario, la contraseña sera incorrecta
+    alert("Contraseña incorrecta.")
+}
