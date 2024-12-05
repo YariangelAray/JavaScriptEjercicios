097c1536b7de02d7919ed8750229f77e37e671fa
Yariangel Aray <gabrielamunar4@gmail.com>
2024-12-04 22:02:52 -0500

    ejercicio 16

--- javascript/ejercicio16.js
+++ javascript/ejercicio16.js
@@ -1,12 +1,15 @@
+
+// Variables
 let km = 2;
 let min = 5;
 
+// funciones para convertir a metros y a segundos
 const convertirMetros = km => km * 1000;
 const convertirSeg = min => min * 60;
-
+// función para calcular la velocidad
 const velocidad = (metros, segundos) => metros/segundos;
-
+// Se llama a las funciones
 let metros = convertirMetros(km);
 let seg = convertirSeg(min);
-
+// Resultado
 alert(`La velocidad del proyectil es de: ${velocidad(metros, seg).toPrecision(3)} m/s.` )
