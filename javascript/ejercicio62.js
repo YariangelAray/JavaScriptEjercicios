cf6e98898dd6b5787766f1f38cbc6d01a9c3622a
Yariangel Aray <gabrielamunar4@gmail.com>
2024-12-09 15:59:40 -0500

    ejercicio 62

---
+++ javascript/ejercicio62.js
@@ -0,0 +1,8 @@
+// pedimos la frase
+let frase = prompt("Ingrese una frase");
+// damos un aviso
+alert("Resultado mostrado en consola.")
+// mostramos la frase 10 veces con un bucle for.
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i}. ${frase}`);
+}
